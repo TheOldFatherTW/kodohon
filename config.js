8f5d56d3c2903142a3114e03310a35e7b0d45dc5
TheOldFatherTW <weslie0705@gmail.com>
2026-09-17 21:33:42 +0800

    Point the public door at the current vault tunnel.

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-window.VAULT_ORIGIN = "https://selected-org-menu-talented.trycloudflare.com";
-window.FAMIGATE_CONFIG = {
-  appId: "kodohon",
-  viewKeyStorage: "kodohon.viewKey",
-  productLabel: "講故事",
-};
+window.VAULT_ORIGIN = "https://coupled-bubble-zero-longer.trycloudflare.com";
